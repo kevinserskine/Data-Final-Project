129a19e84898d5fbbabe92c10e1d35cf6d46308a
Emily Goose <EmilyGoose@users.noreply.github.com>
2021-11-25 20:50:40 -0500

    oops

--- scripts/loginTabs.js
+++ scripts/loginTabs.js
@@ -5,7 +5,7 @@ function loginMode() {
     document.getElementById("loginButton").style.backgroundColor = "#42A5F5";
     document.getElementById("signupButton").style.backgroundColor = "#90CAF9";
 
-    document.getElementById("loginTitle").innerText = "Login to Marketplace";
+    document.getElementById("loginTitle").innerText = "Login to Bookshelf";
 
     // Hide signup only fields
     const elements = document.getElementsByClassName("signup");
@@ -18,12 +18,12 @@ function loginMode() {
 
 function signupMode() {
     // Change the hidden flag for the post request
-    document.getElementById("mode").value = "signup";
+    document.getElementById("mode").value = "register";
     // Swap the button color
     document.getElementById("loginButton").style.backgroundColor = "#90CAF9";
     document.getElementById("signupButton").style.backgroundColor = "#42A5F5";
 
-    document.getElementById("loginTitle").innerText = "Sign up for Marketplace";
+    document.getElementById("loginTitle").innerText = "Sign up for Bookshelf";
 
     // Show signup only fields
     const elements = document.getElementsByClassName("signup");
