419434e08f9d0ca0f0a9e69731f72d73ff2aa6ce
Emily Goose <EmilyGoose@users.noreply.github.com>
2021-11-26 11:11:05 -0500

    css

--- components/imports.php
+++ components/imports.php
@@ -2,6 +2,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <link rel="stylesheet" href="styles/bootstrap.css">
 <link rel="stylesheet" href="styles/index.css">
+<link rel="stylesheet" href="styles/global.css">
 <!-- Fonts -->
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
